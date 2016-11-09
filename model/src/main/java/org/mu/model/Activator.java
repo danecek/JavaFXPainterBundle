87abe297210a76da2aa25f9278810b9f3356ffdc
danecek <danecek@fit.cvut.cz>
2016-11-09 21:02:54 +0100

    Merge origin/master
    Conflicts:
    	.gitignore

--- model/src/main/java/org/mu/model/Activator.java
+++ model/src/main/java/org/mu/model/Activator.java
@@ -8,10 +8,12 @@ public class Activator implements BundleActivator {
 
     private static final Logger LOG = Logger.getLogger(Activator.class.getName());
 
+    @Override
     public void start(BundleContext context) throws Exception {
         LOG.info("");
     }
 
+    @Override
     public void stop(BundleContext context) throws Exception {
         LOG.info("");
     }
